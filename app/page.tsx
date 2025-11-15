31b33bd12171b911fd1bce6b53358a8ddd449dc3
Eric Zhou <ericzhougit@gmail.com>
2025-11-15 10:48:33 -1000

    Figured out a way to read and parse csv

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import * as d3 from "d3";
+import { promises as fs } from 'fs';
 import LinePlot from "./components/test_chart";
 import Sankey from "./components/sankey";
 
 export default async function Page() {
-    const rawData = await d3.csv("https://opendata.hawaii.gov/dataset/d5fa9b76-b63a-4810-93aa-e6624db0fafc/resource/3a0b8368-71a7-4402-b9c7-9fc5a398a952/download/opendata-degrees-awarded.csv");
+    const csvFile = await fs.readFile(process.cwd() + "/app/data/UH_Student_Degrees_Awarded.csv", "utf8");
+    const rawDegreeData = d3.csvParse(csvFile);
     
     return (
         <div>
             <h1>Hello World</h1>
             <LinePlot data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]} />
-            <Sankey data={rawData} />
+            <Sankey />
         </div>
     );
 }
