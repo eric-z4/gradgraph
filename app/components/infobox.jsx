da9d5294349cb631b4397a7e931ea125449aae7c
Eric Zhou <ericzhougit@gmail.com>
2025-11-18 13:26:36 -1000

    Update infobox.jsx

--- app/components/infobox.jsx
+++ app/components/infobox.jsx
@@ -2,6 +2,8 @@ export default function InfoBox({
     className = ""
 })
 {
+    const infoText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut enim ut odio maximus tempor sed sed mi. Curabitur quis tortor sed ex placerat posuere. Donec ipsum ipsum, fringilla non pellentesque in, scelerisque quis lacus. Integer aliquam sem elit, a mattis nibh porta id. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam et maximus velit. In ut urna urna. Nulla eu molestie dui, sed molestie dui. Aenean nisi urna, luctus eu pulvinar vitae, malesuada a arcu.Aliquam erat volutpat.In id laoreet arcu.Proin eget velit massa.Interdum et malesuada fames ac ante ipsum primis in faucibus.Maecenas augue augue, sollicitudin nec aliquet id, iaculis quis odio.In semper justo felis, ac tristique tellus condimentum eget.Duis feugiat risus ipsum, id vehicula nulla ultricies non.Sed iaculis, justo ut elementum suscipit, enim tortor efficitur diam, ac fermentum diam lectus sed ipsum.Pellentesque nec ornare dolor, at suscipit turpis.Nullam commodo ornare metus eget vehicula.Etiam eget est id nulla auctor consequat et at sapien.";
+
     /*
     * - Have text be changeable (Text as useState)
     * - Display info of...
@@ -17,9 +19,7 @@ export default function InfoBox({
                 <p className="text-4xl font-bold">GradGraph</p>
                 <hr></hr>
                 <p className="pt-3 text-left">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut enim ut odio maximus tempor sed sed mi. Curabitur quis tortor sed ex placerat posuere. Donec ipsum ipsum, fringilla non pellentesque in, scelerisque quis lacus. Integer aliquam sem elit, a mattis nibh porta id. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam et maximus velit. In ut urna urna. Nulla eu molestie dui, sed molestie dui.
-
-                    Aenean nisi urna, luctus eu pulvinar vitae, malesuada a arcu. Aliquam erat volutpat. In id laoreet arcu. Proin eget velit massa. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas augue augue, sollicitudin nec aliquet id, iaculis quis odio. In semper justo felis, ac tristique tellus condimentum eget. Duis feugiat risus ipsum, id vehicula nulla ultricies non. Sed iaculis, justo ut elementum suscipit, enim tortor efficitur diam, ac fermentum diam lectus sed ipsum. Pellentesque nec ornare dolor, at suscipit turpis. Nullam commodo ornare metus eget vehicula. Etiam eget est id nulla auctor consequat et at sapien.
+                    {infoText}
                 </p>
             </div>
         </div>
